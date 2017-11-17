2d19533126c341d4eb8c3de3c6fad2f61f763f26
mateusmenezes95 <mateusmenezes95@gmail.com>
2017-11-17 18:31:18 -0300

    Remove interrupt

--- mcc_generated_files/tmr1.c
+++ mcc_generated_files/tmr1.c
@@ -94,8 +94,6 @@ void TMR1_Initialize (void)
     T1CON = 0x8010;
 
     
-    IFS0bits.T1IF = false;
-    IEC0bits.T1IE = true;
 	
     tmr1_obj.timerElapsed = false;
 
@@ -103,22 +101,17 @@ void TMR1_Initialize (void)
 
 
 
-void __attribute__ ( ( interrupt, no_auto_psv ) ) _T1Interrupt (  )
+void TMR1_Tasks_16BitOperation( void )
 {
     /* Check if the Timer Interrupt/Status is set */
-
-    //***User Area Begin
-
-    // ticker function call;
-    // ticker is 1 -> Callback function gets called everytime this ISR executes
-    TMR1_CallBack();
-
-    //***User Area End
-
+    if(IFS0bits.T1IF)
+    {
     tmr1_obj.count++;
     tmr1_obj.timerElapsed = true;
     IFS0bits.T1IF = false;
 }
+}
+
 
 
 void TMR1_Period16BitSet( uint16_t value )
@@ -148,19 +141,12 @@ uint16_t TMR1_Counter16BitGet( void )
 }
 
 
-void __attribute__ ((weak)) TMR1_CallBack(void)
-{
-    // Add your custom callback code here
-//    LED_Toggle();
-}
 
 void TMR1_Start( void )
 {
     /* Reset the status information */
     tmr1_obj.timerElapsed = false;
 
-    /*Enable the interrupt*/
-    IEC0bits.T1IE = true;
 
     /* Start the Timer */
     T1CONbits.TON = 1;
@@ -171,8 +157,6 @@ void TMR1_Stop( void )
     /* Stop the Timer */
     T1CONbits.TON = false;
 
-    /*Disable the interrupt*/
-    IEC0bits.T1IE = false;
 }
 
 bool TMR1_GetElapsedThenClear(void)
